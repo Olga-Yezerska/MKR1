ae72f46046f2cbf65a6ff056bd8ae250ca5a9144
Olga Yezerska <olga_ezerska06@ukr.net>
2026-04-08 15:58:12 +0300

    feat: add load func for file

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from datetime import datetime
+
+FILE = 'tasks.txt'
+
+def load():
+    tasks = []
+    with open(FILE, "r", encoding="utf-8") as f:
+        for line in f:
+            if line.strip():
+                    parts = line.strip().split("|")
+                    if len(parts) == 5:
+                        task_id = int(parts[0])
+                        desc = parts[1]
+                        date = parts[2]
+                        priority = int(parts[3])
+                        done = parts[4] == "True"
+                        tasks.append({
+                            "id": task_id,
+                            "description": desc,
+                            "date": date,
+                            "priority": priority,
+                            "done": done
+                        })
+    return tasks
